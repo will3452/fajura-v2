c2563e528f5607710ff16ab53b4db8233c71ce3f
unknown <williamgalas2@gmail.com>
2021-06-12 15:16:23 +0800

    back button fixed

--- resources/views/services/index.blade.php
+++ resources/views/services/index.blade.php
@@ -2,6 +2,13 @@
 
 @section('content')
     <div class="container">
+        <div class="is-flex is-justify-content-space-between">
+            <a href="{{ route('home') }}" class="button is-small is-rounded has-icon">
+                <div class="icon">
+                    <i data-feather="arrow-left"></i>
+                </div>
+            </a>
+        </div>
         <h2 class="title is-4" style="text-align:center">
             Services
         </h2>
