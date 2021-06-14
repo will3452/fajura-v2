01b3ccbd86038fe8d47767b6f50afa7420ab8b76
unknown <williamgalas2@gmail.com>
2021-06-14 15:10:55 +0800

    add favicons

---
+++ resources/views/includes/favicons.blade.php
@@ -0,0 +1,18 @@
+<link rel="icon" href="/favicons/favicon-16x16.png" sizes="16x16"/>
+<link rel="icon" href="/favicons/favicon-32x32.png" sizes="32x32"/>
+<link rel="icon" href="/favicons/android-icon-72x72.png" sizes="72x72"/>
+<link rel="icon" href="/favicons/favicon-96x96.png" sizes="96x96"/>
+<link rel="icon" href="/favicons/android-icon-144x144.png" sizes="144x144"/>
+<link rel="icon" href="/favicons/android-icon-192x192.png" sizes="192x192"/>
+<link rel="apple-touch-icon" sizes="57x57" href="/favicons/apple-icon-57x57.png">
+<link rel="apple-touch-icon" sizes="60x60" href="/favicons/apple-icon-60x60.png">
+<link rel="apple-touch-icon" sizes="72x72" href="/favicons/apple-icon-72x72.png">
+<link rel="apple-touch-icon" sizes="76x76" href="/favicons/apple-icon-76x76.png">
+<link rel="apple-touch-icon" sizes="114x114" href="/favicons/apple-icon-114x114.png">
+<link rel="apple-touch-icon" sizes="120x12-" href="/favicons/apple-icon-120x12-.png">
+<link rel="apple-touch-icon" sizes="144x144" href="/favicons/apple-icon-144x144.png">
+<link rel="apple-touch-icon" sizes="152x152" href="/favicons/apple-icon-152x152.png">
+<link rel="apple-touch-icon" sizes="180x180" href="/favicons/apple-icon-180x180.png">
+<meta name="msapplication-TileColor" content="#2793DA">
+<meta name="msapplication-TileImage" content="/favicons/ms-icon-144x144.png">
+<link rel="manifest" href="/favicons/manifest.json">
