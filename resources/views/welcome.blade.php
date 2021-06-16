17c07dba59eaef0c8a8818cdf5667142c3711170
William Galas <williamgalas2@gmail.com>
2021-06-16 23:26:23 +0800

    fixed me added

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -34,15 +34,15 @@
                 </li>
             </ul>
         </div>
+        <div class=" fixedme p-2">
+            <button x-on:click="sidebarShow = !sidebarShow" class="no-border button is-rounded">
+                <i data-feather="menu"
+                x-show="!sidebarShow"></i>
+                <i data-feather="x"
+                x-show="sidebarShow"></i>
+            </button>
+        </div>
         <div class="container mobile" >
-            <div class=" fixedme p-2">
-                <button x-on:click="sidebarShow = !sidebarShow" class="no-border button is-rounded">
-                    <i data-feather="menu"
-                    x-show="!sidebarShow"></i>
-                    <i data-feather="x"
-                    x-show="sidebarShow"></i>
-                </button>
-            </div>
             <div id="mobile-hero">
                 <h1 class="title is-2">
                     {{ $app->brand_name }}
@@ -53,6 +53,9 @@
                 <a href="#" id="app_btn" class="button is-rounded is-info" >
                     Book An Appointment
                 </a>
+            </div>
+            <div class="block">
+                Lorem ipsum dolor sit, amet consectetur adipisicing elit. Cupiditate nesciunt repellat fuga voluptates eos cumque nostrum odio sed. Dolorem illum odio molestias omnis similique quis? Alias dignissimos provident dicta eius debitis, eveniet cum quaerat. Animi incidunt inventore, cumque officia optio sit magnam sint nesciunt? Quisquam vel, pariatur molestiae fugiat obcaecati dolores dolorem ipsa reprehenderit eius dolore sunt unde consectetur impedit. Cumque ea sunt numquam suscipit doloremque vel impedit atque perspiciatis natus quisquam repudiandae quas saepe, voluptas magni repellat vero dolores voluptate et nihil, libero quae ab inventore qui. Praesentium quos sed modi dicta cumque assumenda fugiat eveniet culpa nobis. Sapiente, iusto! Quae velit ratione odio similique fugit, accusamus, placeat, vero expedita quidem optio pariatur esse laudantium dolor eum molestiae unde quia blanditiis voluptates minus consectetur deserunt nostrum impedit officia? Atque, adipisci! Consectetur, dicta dolore. Magni excepturi ipsum consequatur ratione atque accusantium est asperiores facilis error eaque architecto, sunt, voluptas veniam. Obcaecati similique commodi repellendus et, odit pariatur? Dignissimos fuga adipisci voluptates, libero eum vel expedita blanditiis error magnam nostrum reprehenderit quo accusamus harum maiores nulla voluptatum, similique ratione eius placeat et optio fugiat est! Cumque cum ex repudiandae saepe consectetur impedit, dignissimos voluptatibus recusandae ullam praesentium hic, odit obcaecati. Magni omnis veniam unde aliquid laboriosam! Perferendis illum ad minima, voluptatum nisi numquam quidem animi repellendus aut esse, accusantium labore ducimus sint sit veritatis voluptate porro quae laboriosam molestiae. Libero nam in aperiam fugiat voluptatem labore aspernatur ipsum dolor, accusamus delectus odit perspiciatis ad eius recusandae vero nihil ipsam fuga dicta fugit non ut? Incidunt nostrum debitis itaque beatae rem velit quos nobis harum distinctio, doloribus asperiores qui aspernatur, fugit non. Omnis, cumque. Aliquid qui blanditiis, repellendus suscipit praesentium nesciunt molestiae quas enim voluptatibus odio possimus rem ipsam repellat ipsa nulla explicabo provident doloribus nobis ab rerum. Cumque aliquid velit consectetur.
             </div>
     
             
