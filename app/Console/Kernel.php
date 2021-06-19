9771c163d8035d2db9f5bca8d33ab1389e89ffba
will3452 <williamgalas2@gmail.com>
2021-06-20 02:39:45 +0800

    fixed

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -28,8 +28,8 @@ class Kernel extends ConsoleKernel
     protected function schedule(Schedule $schedule)
     {
         // $schedule->command('inspire')->hourly();
-        // $schedule->command(NotifyTodaysAppointment::class)->dailyAt('05:00')->timezone('Asia/Manila');
-        $schedule->command(NotifyTodaysAppointment::class)->everyMinute();
+        $schedule->command(NotifyTodaysAppointment::class)->dailyAt('05:00')->timezone('Asia/Manila');
+        // $schedule->command(NotifyTodaysAppointment::class)->everyMinute();
     }
 
     /**
