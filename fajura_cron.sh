d5faf6fff53cb6cbed99719edf45df6442b47a08
will3452 <williamgalas2@gmail.com>
2021-06-20 02:14:36 +0800

    edited cron

--- fajura_cron.sh
+++ fajura_cron.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-/usr/bin/php /home/u903801402/public_html/artisan schedule:run >> /dev/null 2>&1
+/usr/bin/php /home/u903801402/domains/fajura.site/public_html/artisan schedule:run >> /dev/null 2>&1
